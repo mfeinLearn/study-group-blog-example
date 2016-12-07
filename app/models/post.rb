edcb71f6c5e308044d06a83f5bc5346843d84896
Luke Ghenco <lukeghenco@gmail.com>
2016-12-07 06:42:37 -0800

    added Active Record validations and relationships to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  validates :title, :content, presence: true
 end
